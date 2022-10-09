644174d0438366eb0f6fa0accc8e6ffebf08aa9d
fancier21 <fancier@protonmail.com>
2022-10-09 17:27:20 +0400

    movies api

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = []
+
+  getAll(): Movie[] {
+    return this.movies
+  }
+
+  getOne(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id === id)
+    if (!movie) {
+      throw new NotFoundException('Movie is Not Fonded')
+    } else {
+      return movie
+    }
+  }
+
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData
+    })
+  }
+
+  edit(id: number, updateData: UpdateMovieDto) {
+    const movie = this.getOne(id)
+    this.remove(id)
+    this.movies.push({
+      ...movie,
+      ...updateData
+    })
+  }
+
+  remove(id: number) {
+    this.getOne(id)
+    this.movies = this.movies.filter(movie => movie.id !== id)
+  }
+
+}
